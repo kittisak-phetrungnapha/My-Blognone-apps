a721534e8c9d97dc16b32e7bed6b8178df19742c
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2016-12-18 10:32:58 +0700

    First commit files.

---
+++ myblognone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myblognone
+//
+//  Created by Kittisak Phetrungnapha on 12/18/2559 BE.
+//  Copyright © 2559 Kittisak Phetrungnapha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
