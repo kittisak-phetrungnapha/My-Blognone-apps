3a2054e63c0adf4ca30be86412b0290016a1eb92
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2016-12-30 18:33:54 +0700

    Added UI Test target.

---
+++ myblognoneUITests/myblognoneUITests.swift
@@ -0,0 +1,24 @@
+//
+//  myblognoneUITests.swift
+//  myblognoneUITests
+//
+//  Created by Kittisak Phetrungnapha on 12/30/2559 BE.
+//  Copyright © 2559 Kittisak Phetrungnapha. All rights reserved.
+//
+
+import XCTest
+
+class myblognoneUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        continueAfterFailure = false
+        XCUIApplication().launch()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+}
