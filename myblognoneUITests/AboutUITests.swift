87fcbc01160e5171deddeb5d47a1a1f90e11ff19
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2017-01-01 12:43:39 +0700

    Created AboutUITests.

---
+++ myblognoneUITests/AboutUITests.swift
@@ -0,0 +1,43 @@
+//
+//  AboutUITests.swift
+//  myblognone
+//
+//  Created by Kittisak Phetrungnapha on 1/1/2560 BE.
+//  Copyright © 2560 Kittisak Phetrungnapha. All rights reserved.
+//
+
+import XCTest
+
+class AboutUITests: XCTestCase {
+    
+    var app: XCUIApplication!
+    
+    override func setUp() {
+        super.setUp()
+        
+        continueAfterFailure = false
+        app = XCUIApplication()
+        app.launch()
+        XCUIDevice.shared().orientation = .portrait
+        
+        app.buttons[NSLocalizedString("goToAboutNavButton", comment: "")].tap()
+    }
+    
+    override func tearDown() {
+        app = nil
+        super.tearDown()
+    }
+    
+    func testOpenMailForSendFeedback() {
+        let table = app.tables.element
+        waitForElementToAppear(element: table.cells.element)
+        table.cells.element(boundBy: 1).tap()
+    }
+    
+    func testOpenAppStoreForRateApp() {
+        let table = app.tables.element
+        waitForElementToAppear(element: table.cells.element)
+        table.cells.element(boundBy: 2).tap()
+    }
+    
+}
